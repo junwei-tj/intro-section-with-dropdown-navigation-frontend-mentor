2e4c94eb88d595149261505a3c5d8ab892a14f1c
Jun Wei <tohjunwei.tj@gmail.com>
2022-05-11 00:29:50 +0800

    added dropdown menu toggle for sliding nav bar

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ function closeMenu() {
   document.getElementById("side-nav").style.transform = "translateX(100%)";
   document.getElementById("overlay").style.display = "none";
 }
+
+function toggleDropdownMenu(node) {
+  const isShown = node.nextElementSibling.style.display === "flex";
+  node.nextElementSibling.style.display = isShown ? "none" : "flex" ;
+  node.lastElementChild.src = isShown ? "./images/icon-arrow-down.svg" : "./images/icon-arrow-up.svg";
+}
